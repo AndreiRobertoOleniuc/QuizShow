0c8cefe245f003eaac4ced056fd2e06f3f1d7c89
AndreiRobertoOleniuc <andreioleniucroberto@gmail.com>
2021-01-06 07:57:07 +0100

    iniot 2 bitch

---
+++ src/main/java/ch/bbbaden/QuizShow/QuizShowApplication.java
@@ -0,0 +1,13 @@
+package ch.bbbaden.QuizShow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuizShowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuizShowApplication.class, args);
+	}
+
+}
