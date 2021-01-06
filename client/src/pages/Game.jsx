2753c7dfe368f899c090fa18c3aa7582edb9e9a8
AndreiRobertoOleniuc <andreioleniucroberto@gmail.com>
2021-01-06 13:58:49 +0100

    first try

--- client/src/pages/Game.jsx
+++ client/src/pages/Game.jsx
@@ -1,9 +1,34 @@
-import React from 'react'
-
-function Game() {
+import React, { useState, useEffect } from 'react'
+import axios from "axios";
+import Title from "./components/Title";
+import "./style/style.css";
+const Game = () => {
+    const [wort, setWort] = useState("");
+    const [chars, setChars] = useState([]);
+    useEffect(() => {
+        axios.get("http://localhost:8080/")
+            .then((res) => {
+                console.log(res.data);
+                setWort(res.data);
+                setChars([...res.data]);
+                console.log([...res.data]);
+            })
+            .catch((err) => {
+                console.log(err);
+            })
+    }, [])
     return (
-        <div>
-            <h1>Hey shwaty</h1>
+        <div className="gamePage">
+            <Title title={"Wheel of Fortune"} />
+            <div className="wortFeld">
+                <div className="holder">
+                    {chars.map((item) => {
+                        return (
+                            <div className="char">{item}</div>
+                        )
+                    })}
+                </div>
+            </div>
         </div>
     )
 }
--- src/main/java/ch/bbbaden/QuizShow/Controller.java
+++ src/main/java/ch/bbbaden/QuizShow/Controller.java
@@ -1,2 +1,21 @@
-package ch.bbbaden.QuizShow;public class Controller {
+package ch.bbbaden.QuizShow;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+
+@RestController
+public class Controller {
+    ArrayList<String> wörter;
+    public Controller(){
+        wörter = new ArrayList<>();
+        wörter.add("FC Barcelona");
+    }
+    @GetMapping("/")
+    @CrossOrigin(origins = "http://localhost:3000")
+    public String getWortRand(){
+        return wörter.get(0);
+    }
 }
